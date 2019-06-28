d8ad784995ebf0b51f194dd4cc5b0bda61f9bd1a
iamr0man <41066029+iamr0man@users.noreply.github.com>
2019-06-28 17:06:38 +0300

    Add files via upload

--- homework6/js/script.js
+++ homework6/js/script.js
@@ -1,10 +1,10 @@
 const btn = document.createElement('button');
+btn.innerText = 'Change Theme';
+btn.className = 'change-color-btn'
+document.body.appendChild(btn);
+
 const headline = document.querySelectorAll('.headline');
 const container = document.querySelectorAll('.container');
-
-btn.innerText = 'Change Theme';
-btn.className = 'change-color-btn';
-document.body.appendChild(btn);
 
 window.onload = function checkLocal(){
 
@@ -16,18 +16,15 @@ window.onload = function checkLocal(){
 
 btn.addEventListener('click', changeTheme);
 
-function changeTheme() {
-    const current = getComputedStyle(headline.item(0));
-    const blue_text = 'rgb(24, 180, 207)';
-    if (current.color === blue_text) {
-        headline.forEach(e => e.style.color  = '#f2f2f2');
-        container.forEach(e => e.style.backgroundColor = blue_text);
-        localStorage.setItem('color', '#f2f2f2')
-        localStorage.setItem('backgroundColor', blue_text)
+let isNewTheme = false;
+
+function changeTheme(){
+
+    if(!isNewTheme) {
+        document.head.children[3].href = 'css/newTheme.css';
+        isNewTheme = true;
     } else {
-        headline.forEach(e => e.style.color = blue_text);
-        container.forEach(e => e.style.backgroundColor = '#f2f2f2');
-        localStorage.setItem('color', blue_text)
-        localStorage.setItem('backgroundColor', '#f2f2f2')
+        document.head.children[3].href = 'css/style.css';
+        isNewTheme = false;
     }
 }
